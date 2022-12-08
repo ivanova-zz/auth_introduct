4bc35b35fcc83ad9a747f540c10d06eb26a3bb2b
yulia <20051992yuyu@gmail.com>
2022-12-08 12:32:04 +0200

    add const

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AuthIntroduct.MixProject do
   def project do
     [
       app: :auth_introduct,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       package: package(),
