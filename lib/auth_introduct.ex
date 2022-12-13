90b7d7a549086d560a70f684601f41caeb58f175
yulia <20051992yuyu@gmail.com>
2022-12-13 12:26:18 +0200

    code style

--- lib/auth_introduct.ex
+++ lib/auth_introduct.ex
@@ -5,7 +5,6 @@ defmodule AuthIntroduct do
   import AuthIntroduct.User
 
   defmacro __using__(options) do
-    IO.puts("options in using: #{inspect options}")
     behaviour = get_module(Keyword.get(options, :module))
     aud = Keyword.get(options, :aud)
     iss = Keyword.get(options, :iss)
@@ -31,29 +30,6 @@ defmodule AuthIntroduct do
         else
           e -> conn |> resp(401, "#{inspect e}") |> halt()
         end
-
-
-#          user_id = conn.body_params[options[:key]]
-#          token = get_token(conn.req_headers)
-#          IO.puts("authorization: #{inspect token}")
-#          IO.puts("options_call: #{inspect options}")
-#          secret_key = get_secret_key
-#          IO.puts("secret_key: #{inspect secret_key}")
-#          {:ok, jwt_body} = verify_jwt(token, secret_key, @aud, @iss)
-#          IO.puts("verify_jwt: #{inspect jwt_body}")
-#          {:ok, sub} = Map.fetch(jwt_body, "sub")
-#          {:ok, role} = Map.fetch(jwt_body, "role")
-#
-#          IO.puts("sub: #{inspect sub}")
-#          user = Mod.get_user(user_id)
-#          answer = validate_user(user_id, sub, user, role, Mod.get_role(user.role_id))
-#          IO.puts("validate_user: #{inspect answer}")
-#          if {:ok, :authorized} == answer do
-#            conn
-#          else
-#            conn |> resp(401, "unauthorized") |> halt()
-#          end
-
       end
 
       def generate_token(conn, opt) do
